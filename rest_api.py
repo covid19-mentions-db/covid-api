f0c5bdbbd5841c822940dcfddf50751b1889e985
covid <dev@covid19-mentions-db.com>
2020-04-29 13:45:44 +0500

    some minor changes - for convenience

--- rest_api.py
+++ rest_api.py
@@ -9,18 +9,18 @@ api = Api(app)
 class Search(Resource):
     def __init__(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('source', type=str, choices=['twitter', 'instagram', 'facebook'])
+        parser.add_argument('source', type=str, choices=['twitter', 'instagram', 'facebook', ''])
         parser.add_argument('author_id', type=str)
         parser.add_argument('object_text', type=str)
         parser.add_argument('language', type=str)
         parser.add_argument('keyword', type=str)
         parser.add_argument('location', type=str)
-        parser.add_argument('lat', type=float, default=None)
-        parser.add_argument('lon', type=float, default=None)
-        parser.add_argument('distance', type=int, default=1000)
-        parser.add_argument('time_start', type=int)
-        parser.add_argument('time_end', type=int)
-        parser.add_argument('limit', type=int, default=12)
+        parser.add_argument('lat', type=str)
+        parser.add_argument('lon', type=str)
+        parser.add_argument('distance', type=str, default='1000')
+        parser.add_argument('time_start', type=str)
+        parser.add_argument('time_end', type=str)
+        parser.add_argument('limit', type=str, default='12')
 
         args = parser.parse_args()
 
@@ -30,15 +30,41 @@ class Search(Resource):
         self.language = args['language']
         self.keyword = args['keyword']
         self.location = args['location']
-        self.lat = args['lat']
-        self.lon = args['lon']
-        self.distance = args['distance']
-        self.time_start = args['time_start']
-        self.time_end = args['time_end']
 
-        self.limit = args['limit']
-        if self.limit > 10000:
-            self.limit = 10000
+        _lat = args['lat']
+        if _lat:
+            self.lat = float(_lat)
+        else:
+            self.lat = None
+        _lon = args['lon']
+        if _lon:
+            self.lon = float(_lon)
+        else:
+            self.lon = None
+        _distance = args['distance']
+        if _distance:
+            self.distance = int(_distance)
+        else:
+            self.distance = 1000
+
+        _time_start = args['time_start']
+        if _time_start:
+            self.time_start = int(_time_start)
+        else:
+            self.time_start = None
+        _time_end = args['time_end']
+        if _time_end:
+            self.time_end = int(_time_start)
+        else:
+            self.time_end = None
+
+        _limit = args['limit']
+        if _limit:
+            self.limit = int(_limit)
+            if self.limit > 10000:
+                self.limit = 10000
+        else:
+            self.limit = 12
 
         super(Search, self).__init__()
 
